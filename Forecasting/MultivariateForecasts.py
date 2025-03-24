Corrected prediction interval calculation and generalized forecast visualization methods

Forecasting/MultivariateForecasts.py
@@ -11,9 +11,7 @@ def multiseries_independent_forecasts(
         countries: list[str],
         train_split: float,
         start_year: int,
-        horizon: int,
-        lower_quantile: float,
-        upper_quantile: float    
+        horizon: int, 
     ):
     """
     Performs probabilistic forecasting on multiple time series without considering the relations between different time series
@@ -24,8 +22,6 @@ def multiseries_independent_forecasts(
         train_split (float): The split between train and test set (must be in (0,1))
         start_year (int): The start year of the time series
         horizon (int): The number of future values to be predicted
-        lower_quantile (float): The lower bound quantile to be predicted
-        upper_quantile (float): The upper bound quantile to be predicted
 
     Returns:
         pd.DataFrame: The indexed training set
@@ -42,19 +38,16 @@ def multiseries_independent_forecasts(
     forecaster, _ = ForecastingUtils.grid_search_multiple_inputs(
         data_train=data_train,
         data_test=data_test,
-        lags_bound=4,
-        difference_bound=2,
-        ma_bound=3,
-        lower_bound=lower_quantile,
-        upper_bound=upper_quantile,
+        lags_bound=1,
+        difference_bound=0,
+        ma_bound=0,
         countries_to_predict=countries, 
         model_type='ForecasterRecursiveMultiSeries',
         horizon=horizon
     )
 
     forecaster.fit(series=data_train)
-    test_preds = forecaster.predict_interval(steps=test_steps, interval=[
-            lower_quantile, upper_quantile], n_boot=100)
+    test_preds = forecaster.predict_quantiles(steps=test_steps, quantiles=[0.05,0.16,0.84,0.95], n_boot=100)
     test_med = forecaster.predict_quantiles(steps=test_steps, quantiles=[0.5])
     test_mean = ForecastingUtils.predict_mean(
         forecaster=forecaster, to_predict=countries, horizon=test_steps, univariate=False
@@ -62,8 +55,7 @@ def multiseries_independent_forecasts(
     test_preds = pd.concat([test_preds, test_med, test_mean], axis=1)
 
     forecaster.fit(series=data_all)
-    horizon_preds = forecaster.predict_interval(steps=horizon, interval=[
-            lower_quantile, upper_quantile], n_boot=100)
+    horizon_preds = forecaster.predict_quantiles(steps=horizon, quantiles=[0.05,0.16,0.84,0.95], n_boot=100)
     horizon_med = forecaster.predict_quantiles(steps=horizon, quantiles=[0.5])
     horizon_mean = ForecastingUtils.predict_mean(
         forecaster=forecaster, to_predict=countries, horizon=horizon, univariate=False
@@ -80,8 +72,6 @@ def many_to_one_forecasts(
         train_split: float,
         start_year: int,
         horizon: int,
-        lower_quantile: float,
-        upper_quantile: float,
         countries_to_predict: list[str] = None 
     ):
     """
@@ -93,8 +83,6 @@ def many_to_one_forecasts(
         train_split (float): The split between train and test set (must be in (0,1))
         start_year (int): The start year of the time series
         horizon (int): The number of future values to be predicted
-        lower_quantile (float): The lower bound quantile to be predicted
-        upper_quantile (float): The upper bound quantile to be predicted
         countries_to_predict (list[str]): The codes of countries for which predictions should be made. If None, predictions for the entire dataset are performed
         
     Returns:
@@ -105,7 +93,6 @@ def many_to_one_forecasts(
         pd.DataFrame: The in-sample prediction intervals
     """
     T = y.shape[1]
-    T_in = pd.date_range(start=f'{start_year+T-horizon}', end=f'{start_year+T}')
     T_horizon = pd.date_range(start=f'{start_year+T}', end=f'{start_year+T+horizon}', freq='Y')
  
     test_steps, data_train, data_test, data_all = PreProcessing.preprocess_multivariate_forecast(
@@ -114,31 +101,28 @@ def many_to_one_forecasts(
 
     test_preds = pd.DataFrame(index=data_test.index)
     horizon_preds = pd.DataFrame(index=T_horizon)
-    in_sample_preds = pd.DataFrame(index=T_in)
 
     if countries_to_predict is not None:
         to_predict = countries_to_predict
     else:
         to_predict = countries
-
+    in_sample = []
     for country in to_predict:
         test_forecaster, horizon_forecaster = ForecastingUtils.grid_search_multiple_inputs(
             data_train=data_train,
             data_test=data_test,
-            lags_bound=4,
-            difference_bound=2,
-            ma_bound=3,
-            lower_bound=lower_quantile,
-            upper_bound=upper_quantile,
+            lags_bound=1,
+            difference_bound=0,
+            ma_bound=0,
             countries_to_predict=[country],
             model_type='ForecasterDirectMultiVariate',
             horizon=horizon
         )
 
         test_forecaster.fit(series=data_train)
-        country_test_preds = test_forecaster.predict_interval(
+        country_test_preds = test_forecaster.predict_quantiles(
             steps=test_steps,
-            interval=[lower_quantile, upper_quantile],
+            quantiles=[0.05,0.16,0.84,0.95],
             n_boot = 100
         )
         country_test_med = test_forecaster.predict_quantiles(steps=test_steps, quantiles=[0.5])
@@ -148,9 +132,9 @@ def many_to_one_forecasts(
         country_test_preds = pd.concat([country_test_preds, country_test_med, country_test_mean], axis=1)
 
         horizon_forecaster.fit(series=data_all)
-        country_horizon_preds = horizon_forecaster.predict_interval(
+        country_horizon_preds = horizon_forecaster.predict_quantiles(
             steps=horizon,
-            interval=[lower_quantile, upper_quantile],
+            quantiles=[0.05,0.16,0.84,0.95],
             n_boot = 100
         )
         country_horizon_med = horizon_forecaster.predict_quantiles(steps=horizon, quantiles=[0.5])
@@ -159,12 +143,12 @@ def many_to_one_forecasts(
         )
         country_horizon_preds = pd.concat([country_horizon_preds, country_horizon_med, country_horizon_mean], axis=1)
         country_in_sample_preds = ForecastingUtils.predict_in_sample(data_train, horizon_forecaster)
-
+        in_sample.append(country_in_sample_preds)
         test_preds = pd.concat([test_preds, country_test_preds], axis=1)
         horizon_preds = pd.concat([horizon_preds, country_horizon_preds], axis=1)
-        in_sample_preds = pd.concat([in_sample_preds, country_in_sample_preds], axis=1)
+        
 
-    return data_train, data_test, test_preds, horizon_preds
+    return data_train, data_test, test_preds, horizon_preds, pd.concat(in_sample, axis=1)
 
 def many_to_many_forecasts(
         y: np.ndarray,
@@ -172,8 +156,6 @@ def many_to_many_forecasts(
         train_split: float,
         start_year: int,
         horizon: int,
-        lower_quantile: float,
-        upper_quantile: float,
         countries_to_predict: list[str] = None 
     ):
     """
@@ -185,8 +167,6 @@ def many_to_many_forecasts(
         train_split (float): The split between train and test set (must be in (0,1))
         start_year (int): The start year of the time series
         horizon (int): The number of future values to be predicted
-        lower_quantile (float): The lower bound quantile to be predicted
-        upper_quantile (float): The upper bound quantile to be predicted
         countries_to_predict (list[str]): The codes of countries for which predictions should be made. If None, predictions for the entire dataset are performed
         
     Returns:
@@ -209,11 +189,9 @@ def many_to_many_forecasts(
     test_forecaster, horizon_forecaster = ForecastingUtils.grid_search_multiple_inputs(
         data_train=data_train,
         data_test=data_test,
-        lags_bound=4,
-        difference_bound=2,
-        ma_bound=3,
-        lower_bound=lower_quantile,
-        upper_bound=upper_quantile,
+        lags_bound=1,
+        difference_bound=0,
+        ma_bound=1,
         countries_to_predict=to_predict,
         model_type='ForecastDirectMultiOutput',
         horizon=horizon
@@ -221,9 +199,9 @@ def many_to_many_forecasts(
 
     test_forecaster.fit(series=data_train)
 
-    test_preds = test_forecaster.predict_interval(
+    test_preds = test_forecaster.predict_quantiles(
         steps=test_steps, 
-        interval=[lower_quantile, upper_quantile], 
+        quantiles=[0.05,0.16,0.84,0.95], 
         n_boot=100
     )
 
@@ -235,9 +213,9 @@ def many_to_many_forecasts(
 
     horizon_forecaster.fit(series=data_all)
 
-    horizon_preds = horizon_forecaster.predict_interval(
+    horizon_preds = horizon_forecaster.predict_quantiles(
         steps=horizon,
-        interval=[lower_quantile, upper_quantile],
+        quantiles=[0.05,0.16,0.84,0.95],
         n_boot = 100
     )
 
@@ -257,8 +235,6 @@ def rnn_forecasts(
         train_split: float,
         start_year: int,
         horizon: int,
-        lower_quantile: float,
-        upper_quantile: float,
         differentiation: int = None,
         countries_to_predict: list[str] = None 
     ):
@@ -273,8 +249,6 @@ def rnn_forecasts(
         train_split (float): The split between train and test set (must be in (0,1))
         start_year (int): The start year of the time series
         horizon (int): The number of future values to be predicted
-        lower_quantile (float): The lower bound quantile to be predicted
-        upper_quantile (float): The upper bound quantile to be predicted
         differentiation (int): The order of differentiation
         countries_to_predict (list[str]): The codes of countries for which predictions should be made. If None, predictions for the entire dataset are performed
         
@@ -324,21 +298,19 @@ def rnn_forecasts(
         data_train=data_train,
         data_test=data_test,
         data_all=data_all,
-        lags_bound=4,
+        lags_bound=1,
         layer_type=layer_type,
-        recurrent_layers = np.array([4,8,16,32]),
-        dense_layers = np.array([16,32,64]),
-        lower_bound=lower_quantile,
-        upper_bound=upper_quantile,
+        recurrent_layers = np.array([4]),
+        dense_layers = np.array([16]),
         countries_to_predict=to_predict,
         horizon=horizon
     )
 
     test_forecaster.fit(series=data_train)
 
-    test_preds = test_forecaster.predict_interval(
+    test_preds = test_forecaster.predict_quantiles(
         steps=test_steps, 
-        interval=[lower_quantile, upper_quantile], 
+        quantiles=[0.05, 0.16, 0.84, 0.95], 
         n_boot=100
     )
 
@@ -357,9 +329,9 @@ def rnn_forecasts(
 
     horizon_forecaster.fit(series=data_all)
 
-    horizon_preds = horizon_forecaster.predict_interval(
+    horizon_preds = horizon_forecaster.predict_quantiles(
         steps=horizon,
-        interval=[lower_quantile, upper_quantile],
+        quantiles=[0.05, 0.16, 0.84, 0.95],
         n_boot = 100
     )
 
@@ -380,4 +352,4 @@ def rnn_forecasts(
                 country
             ].inverse_transform_training(in_sample_preds[col].values)
 
-    return data_train_pure, data_test_pure, test_preds, horizon_preds
+    return data_train_pure, data_test_pure, test_preds, horizon_preds, in_sample_preds

Forecasting/UnivariateForecasts.py
@@ -2,28 +2,20 @@ import numpy as np
 
 import pandas as pd
 
-from skforecast.recursive import ForecasterSarimax
-from skforecast.sarimax import Sarimax
-
 from Utils import ForecastingUtils, PreProcessing
 
 def univariate_forecast(
         y: np.ndarray,
-        arima_order: np.ndarray,
         train_split: float,
         start_year: int,
         horizon: int,
-        lower_quantile: float,
-        upper_quantile: float,
-        country: str,
-        use_gbr: float = True,
+        country: str
     ):
     """
     Performs probabilistic forecasting on a single univariate time series, using Gradient Boosting
 
     Parameters:
         y (np.ndarray): The input time series of length T
-        arima_order (np.ndarray): The (p,d,q) ARIMA orders of y
         train_split (float): The split between train and test set (must be in (0,1))
         start_year (int): The start year of the time series
         horizon (int): The number of future values to be predicted
@@ -39,41 +31,24 @@ def univariate_forecast(
         pd.DataFrame: The prediction intervals for the horizon
         pd.DataFrame: The in-sample prediction intervals
     """
-    p = int(arima_order[0])
-    d = int(arima_order[1])
-    q = int(arima_order[2])
 
     test_steps, data_train, data_test, data_all = PreProcessing.preprocess_univariate_forecast(
         y = y, start_year = start_year, train_split = train_split
     )
 
-    if use_gbr:
-        forecaster = ForecastingUtils.grid_search_univariate(
-            data_train=data_train,
-            data_test=data_test,
-            lags_bound=4,
-            difference_bound=2,
-            ma_bound=3,
-            lower_bound=lower_quantile,
-            upper_bound=upper_quantile,
-            country=country
-        )
-    else:
-        forecaster = ForecasterSarimax(regressor = Sarimax(order=(p,d,q)))
+    forecaster = ForecastingUtils.grid_search_univariate(
+        data_train=data_train,
+        data_test=data_test,
+        lags_bound=1,
+        difference_bound=0,
+        ma_bound=1,
+        country=country
+    )
 
     forecaster.fit(y=data_train)
-    if use_gbr:
-        test_preds = forecaster.predict_interval(steps=test_steps, interval=[
-            lower_quantile, upper_quantile], n_boot=100)
-        test_med = forecaster.predict_quantiles(steps=test_steps, quantiles=[0.5])
-        test_med.columns = ['median']
-    else:
-        test_preds = forecaster.predict_interval(steps=test_steps, interval=[
-            lower_quantile, upper_quantile])
-        # not actual median; just for consistency
-        test_med = (test_preds['lower_bound']+test_preds['upper_bound'])/2
-        test_med.name = 'median'
-        test_med = pd.DataFrame(test_med)
+    test_preds = forecaster.predict_quantiles(steps=test_steps, quantiles=[0.05, 0.16, 0.84, 0.95], n_boot=100)
+    test_med = forecaster.predict_quantiles(steps=test_steps, quantiles=[0.5])
+    test_med.columns = ['median']
 
     test_mean = ForecastingUtils.predict_mean(
         forecaster=forecaster, to_predict=['NA'], horizon=test_steps, univariate=True
@@ -83,17 +58,9 @@ def univariate_forecast(
     test_preds = pd.concat([test_preds, test_med, test_mean], axis=1)
 
     forecaster.fit(y=data_all)
-    if use_gbr:
-        horizon_preds = forecaster.predict_interval(steps=horizon, interval=[
-            lower_quantile, upper_quantile], n_boot=100)
-        horizon_med = forecaster.predict_quantiles(steps=horizon, quantiles=[0.5])
-        horizon_med.columns = ['median']
-    else:
-        horizon_preds = forecaster.predict_interval(steps=horizon, interval=[
-            lower_quantile, upper_quantile])
-        horizon_med = (horizon_preds['lower_bound']+horizon_preds['upper_bound'])/2
-        horizon_med.name = 'median'
-        horizon_med = pd.DataFrame(horizon_med)
+    horizon_preds = forecaster.predict_quantiles(steps=horizon, quantiles=[0.05, 0.16, 0.84, 0.95], n_boot=100)
+    horizon_med = forecaster.predict_quantiles(steps=horizon, quantiles=[0.5])
+    horizon_med.columns = ['median']
 
     horizon_mean = ForecastingUtils.predict_mean(
         forecaster=forecaster, to_predict=['NA'], horizon=horizon, univariate=True
@@ -103,5 +70,12 @@ def univariate_forecast(
     horizon_preds = pd.concat([horizon_preds, horizon_med, horizon_mean], axis=1)
 
     in_sample_preds = ForecastingUtils.predict_in_sample(data_train, forecaster)
+
+    columns = [f'{country}_q_0.05', f'{country}_q_0.16', f'{country}_q_0.84', f'{country}_q_0.95']
+    in_sample_preds.columns = columns
+    columns.append(f'{country}_q_0.5')
+    columns.append(f'{country}_mean')
+    test_preds.columns = columns
+    horizon_preds.columns=columns
 
     return data_train, data_test, test_preds, horizon_preds, in_sample_preds

Utils/DataUtils.py
@@ -189,24 +189,29 @@ def load_forecast(
         horizon (int): The number of future values to be predicted
     
     Returns:
+        pd.DataFrame: The in-sample predictions
         pd.DataFrame: The predictions on the test set
         pd.DataFrame: The predictions on the horizon
     """
 
+    in_path = f'{dir}/in_sample.csv'
     test_path = f'{dir}/test.csv'
     horizon_path = f'{dir}/future.csv'
     
+    in_sample = pd.read_csv(in_path)
     test_preds = pd.read_csv(test_path)
     horizon_preds = pd.read_csv(horizon_path)
 
     split_ind = int(train_split*T)
+    T_in_sample = pd.date_range(start=f'{start_year+split_ind-len(in_sample)}', end=f'{start_year+split_ind}', freq='Y')
     T_test = pd.date_range(start=f'{start_year+split_ind}', end=f'{start_year+T}', freq='Y')
     T_horizon = pd.date_range(start=f'{start_year+T}', end=f'{start_year+T+horizon}', freq='Y')
 
+    in_sample.index = T_in_sample
     test_preds.index = T_test
     horizon_preds.index = T_horizon
 
-    return test_preds, horizon_preds
+    return in_sample, test_preds, horizon_preds
 
 def load_baseline_data(
         n: int
@@ -275,6 +280,7 @@ def write_labels(
         writer.writerows(rows)
 
 def write_forecasts(
+        in_sample: pd.DataFrame,
         test_preds: pd.DataFrame,
         horizon_preds: pd.DataFrame,
         dir: str    
@@ -283,19 +289,21 @@ def write_forecasts(
     Writes predictions for test and horizon in different files
 
     Parameters:
+        in_sample (pd.DataFrame): The in-sample predictions
         test_preds (pd.DataFrame): The predictions on the test set
         horizon_preds (pd.DataFrame): The future predictions
         dir (str): The path to the directory to store the files
     """
+    in_path = f"{dir}/in_sample.csv"
     test_path = f"{dir}/test.csv"
     horizon_path = f"{dir}/future.csv"
+    in_sample.to_csv(in_path, index=False)
     test_preds.to_csv(test_path, index=False)
     horizon_preds.to_csv(horizon_path, index=False)
 
 def select_predictions(
         country: str, 
-        predictions: pd.DataFrame,
-        only_bounds: bool = False
+        predictions: pd.DataFrame
     ) -> pd.DataFrame:
     """
     Select and isolate the predictions of a specified country
@@ -303,68 +311,13 @@ def select_predictions(
     Parameters:
         country (str): The ISO-3 code of the specified country
         predictions (pd.DataFrame): The object containing predictions for multiple countries
-        only_bounds (bool): Whether or not to only take the upper and lower bounds
         
     Returns:
         pd.DataFrame: The predictions for the selected country 
     """
-    if only_bounds:
-        new_cols = {
-            country: country,
-            f'{country}_lower_bound':'lower_bound',
-            f'{country}_upper_bound':'upper_bound',
-        }
-    else:
-        new_cols = {
-            country: country,
-            f'{country}_lower_bound':'lower_bound',
-            f'{country}_upper_bound':'upper_bound',
-            f'{country}_q_0.5': 'median',
-            f'{country}_mean': 'mean'
-        }
-
-    if only_bounds:
-        selected_preds = predictions[[
-            country,f'{country}_lower_bound', f'{country}_upper_bound'
-        ]].rename(columns=new_cols)    
-    else:
-        selected_preds = predictions[[
-            country,f'{country}_lower_bound', f'{country}_upper_bound', f'{country}_q_0.5', f'{country}_mean'
-        ]].rename(columns=new_cols)
-
-    return selected_preds
-
-def select_baseline(
-        country: str,
-        stats: pd.DataFrame,
-        use_67: bool = True    
-    ) -> dict:
-    """
-    Selects the baseline predictions for a given country
-
-    Parameters:
-        country (str): The ISO-3 code of the country to select
-        stats (pd.DataFrame): The dataframe containing prediction statistics
-        use_67 (bool): Whether or not to use the 67% prediction intervals; if False, 90% prediction intervals are used
+    new_cols = []
+    for col in predictions.columns:
+        if country in col:
+            new_cols.append(col)
     
-    Returns:
-        dict: A dictionary containing the bounds, mean and median for the final step of the horizon
-    """
-    if use_67:
-        lower = 0.16
-        upper = 0.84
-    else:
-        lower = 0.05
-        upper = 0.95
-
-    row = stats.loc[country]
-    curr = row['2017Value']
-    results = {
-        'curr': curr,
-        'lower_bound': np.log(row[lower])+curr,
-        'upper_bound': np.log(row[upper])+curr,
-        'median': np.log(row[0.5])+curr,
-        'mean': np.log(row['Mean'])+curr
-    }
-
-    return results
+    return predictions[new_cols]

Utils/ForecastingUtils.py
@@ -139,19 +139,20 @@ def calculate_metrics(
 
     return metrics
 
-def interval_width(preds: pd.DataFrame, country: str) -> pd.DataFrame:
+def interval_width(lower_bound: pd.Series, upper_bound: pd.Series, country: str) -> pd.DataFrame:
     """
     Calculates the length of the prediction intervals for all predicted samples for a specific country
 
     Parameters:
-        preds (pd.DataFrame): The predictions for the specified country
+        lower_bound (pd.Series): The lower bound of the interval
+        upper_bound (pd.Series): The upper bound of the interval
         country (str): The country in question
     
     Returns:
         pd.DataFrame: The data frame containing the interval lengths per annum
     """
 
-    intervals = pd.DataFrame(data=preds['upper_bound']-preds['lower_bound'], columns=[country])
+    intervals = pd.DataFrame(data=upper_bound-lower_bound, columns=[country])
     return intervals
 
 def evaluate(
@@ -335,6 +336,8 @@ def predict_in_sample(data_train: pd.DataFrame, forecaster: ForecasterBase) -> p
     remaining_steps = len(data_train)-size
     first_window = data_train[:size]
     all_preds = forecaster.predict_quantiles(steps = remaining_steps, last_window=first_window, quantiles = [0.05, 0.16, 0.84, 0.95])
+    if isinstance(forecaster, ForecasterDirectMultiVariate):
+        return all_preds
     return all_preds[:remaining_steps]
 
 def create_univariate_forecaster(
@@ -460,8 +463,6 @@ def grid_search_univariate(
         lags_bound: int,
         difference_bound: int,
         ma_bound: int,
-        lower_bound: int,
-        upper_bound: int,
         country: str   
     ) -> ForecasterRecursive:
     """
@@ -473,8 +474,6 @@ def grid_search_univariate(
         lags_bound (int): The maximum number of lags to consider
         difference_bound (int): The maximum differentiation order to consider
         ma_bound (int): The maximum rolling window size to consider
-        lower_quantile (float): The lower bound quantile to be predicted
-        upper_quantile (float): The upper bound quantile to be predicted
         country (str): The country for which predictions are made
         
     Returns:
@@ -492,8 +491,8 @@ def grid_search_univariate(
     A, B, C = np.meshgrid(p_list, d_list, q_list, indexing='ij')
     configurations = np.stack([A.ravel(), B.ravel(), C.ravel()], axis=-1)
 
-    interval_lengths = np.zeros(len(configurations))
-    cov = np.zeros(len(configurations))
+    interval_lengths = np.zeros((len(configurations), 2))
+    cov = np.zeros((len(configurations),2))
 
     for i, config in enumerate(configurations):
         p = int(config[0])
@@ -501,13 +500,15 @@ def grid_search_univariate(
         q = int(config[2])
         forecaster = create_univariate_forecaster(p, d, q)
         forecaster.fit(y=data_train)
-        preds = forecaster.predict_interval(steps=test_steps, interval=[
-            lower_bound, upper_bound], n_boot=100)
-        pred_lengths = interval_width(preds, country)
-        interval_lengths[i] = pred_lengths.iloc[-1, 0]
-        cov[i] = probability_coverage(y, preds['lower_bound'], preds['upper_bound'])
+        preds = forecaster.predict_quantiles(steps=test_steps, quantiles=[0.05, 0.16, 0.84, 0.95], n_boot=100)
+        pred_lengths67 = interval_width(preds['q_0.16'], preds['q_0.84'], country)
+        pred_lengths90 = interval_width(preds['q_0.05'], preds['q_0.95'], country)
+        interval_lengths[i][0] = pred_lengths67.iloc[-1, 0]
+        interval_lengths[i][1] = pred_lengths90.iloc[-1, 0]
+        cov[i][0] = probability_coverage(y, preds['q_0.16'], preds['q_0.84'])
+        cov[i][1] = probability_coverage(y, preds['q_0.05'], preds['q_0.95'])
 
-    cov_ratios = cov/interval_lengths
+    cov_ratios = cov[:,0]/interval_lengths[:,0]+cov[:,1]/interval_lengths[:,1]
     ind = np.argmax(cov_ratios)
     p = int(configurations[ind][0])
     d = int(configurations[ind][1])
@@ -521,8 +522,6 @@ def grid_search_multiple_inputs(
         lags_bound: int,
         difference_bound: int,
         ma_bound: int,
-        lower_bound: int,
-        upper_bound: int,
         countries_to_predict: list[str],
         model_type: str,
         horizon: int        
@@ -536,8 +535,6 @@ def grid_search_multiple_inputs(
         lags_bound (int): The maximum number of lags to consider
         difference_bound (int): The maximum differentiation order to consider
         ma_bound (int): The maximum rolling window size to consider
-        lower_quantile (float): The lower bound quantile to be predicted
-        upper_quantile (float): The upper bound quantile to be predicted
         countries_to_predict (list[str]): The countries for which predictions are made
         model_type (str): The type of model to create. One of 'ForecasterRecursiveMultiSeries', 'ForecasterDirectMultiVariate', 'ForecastDirectMultiOutput'
         horizon (int): The number of future values to ultimately predict
@@ -560,8 +557,8 @@ def grid_search_multiple_inputs(
     configurations = np.stack([grid.ravel() for grid in grids], axis=-1)
 
     no_configs = len(configurations)
-    interval_lengths = np.zeros((no_configs, m))
-    cov = np.zeros((no_configs, m))
+    interval_lengths = np.zeros((no_configs, m, 2))
+    cov = np.zeros((no_configs, m, 2))
 
     for i, config in enumerate(configurations):
         p = int(config[0])
@@ -578,16 +575,17 @@ def grid_search_multiple_inputs(
         )
 
         forecaster.fit(series = data_train)
-        preds = forecaster.predict_interval(steps=test_steps, interval=[
-            lower_bound, upper_bound], n_boot=100)
+        preds = forecaster.predict_quantiles(steps=test_steps, quantiles=[0.05, 0.16, 0.84, 0.95], n_boot=100)
         for j, country in enumerate(countries_to_predict):
             y = data_test[country]
-            selected = DataUtils.select_predictions(country, preds, only_bounds=True)
-            pred_lengths = interval_width(selected, country)
-            interval_lengths[i,j] = pred_lengths.iloc[-1,0]
-            cov[i,j] = probability_coverage(y, selected['lower_bound'], selected['upper_bound'])
+            pred_lengths67 = interval_width(preds[f'{country}_q_0.16'], preds[f'{country}_q_0.84'], country)
+            pred_lengths90 = interval_width(preds[f'{country}_q_0.05'], preds[f'{country}_q_0.95'], country)
+            interval_lengths[i][j][0] = pred_lengths67.iloc[-1, 0]
+            interval_lengths[i][j][1] = pred_lengths90.iloc[-1, 0]
+            cov[i][j][0] = probability_coverage(y, preds[f'{country}_q_0.16'], preds[f'{country}_q_0.84'])
+            cov[i][j][1] = probability_coverage(y, preds[f'{country}_q_0.05'], preds[f'{country}_q_0.95'])
     
-    cov_ratios = cov/interval_lengths
+    cov_ratios = cov[:,:,0]/interval_lengths[:,:,0]+cov[:,:,1]/interval_lengths[:,:,1]
     means = np.mean(cov_ratios, axis=1)
     ind = np.argmax(means)
     config = configurations[ind]
@@ -623,8 +621,6 @@ def grid_search_rnn(
         layer_type: str,
         recurrent_layers: np.ndarray,
         dense_layers: np.ndarray,
-        lower_bound: int,
-        upper_bound: int,
         countries_to_predict: list[str],
         horizon: int
     ) -> Tuple[ForecastRNNProb, ForecastRNNProb]:
@@ -639,8 +635,6 @@ def grid_search_rnn(
         layer_type (str): The type of recurrent layer to be used, either LSTM or RNN
         recurrent_layers (np.ndarray): The possible numbers of recurrent units
         dense_layers (np.ndarray): The possible numbers of dense units
-        lower_quantile (float): The lower bound quantile to be predicted
-        upper_quantile (float): The upper bound quantile to be predicted
         countries_to_predict (list[str]): The countries for which predictions are made
         model_type (str): The type of model to create. One of 'ForecasterRecursiveMultiSeries', 'ForecasterDirectMultiVariate', 'ForecastDirectMultiOutput'
         horizon (int): The number of future values to ultimately predict
@@ -659,8 +653,8 @@ def grid_search_rnn(
     grids = np.meshgrid(*arrays, indexing='ij')
     configurations = np.stack([grid.ravel() for grid in grids], axis=-1)
 
-    interval_lengths = np.zeros((len(configurations), m))
-    cov = np.zeros((len(configurations), m))
+    interval_lengths = np.zeros((len(configurations), m, 2))
+    cov = np.zeros((len(configurations), m, 2))
 
     for i, config in enumerate(configurations):
         p = int(config[0])
@@ -678,16 +672,17 @@ def grid_search_rnn(
         )
 
         forecaster.fit(series = data_train)
-        preds = forecaster.predict_interval(steps=test_steps, interval=[
-            lower_bound, upper_bound], n_boot=100)
+        preds = forecaster.predict_quantiles(steps=test_steps, quantiles=[0.05, 0.16, 0.84, 0.95], n_boot=100)
         for j, country in enumerate(countries_to_predict):
             y = data_test[country]
-            selected = DataUtils.select_predictions(country, preds, only_bounds=True)
-            pred_lengths = interval_width(selected, country)
-            interval_lengths[i,j] = pred_lengths.iloc[-1,0]
-            cov[i,j] = probability_coverage(y, selected['lower_bound'], selected['upper_bound'])
-
-    cov_ratios = cov/interval_lengths
+            pred_lengths67 = interval_width(preds[f'{country}_q_0.16'], preds[f'{country}_q_0.84'], country)
+            pred_lengths90 = interval_width(preds[f'{country}_q_0.05'], preds[f'{country}_q_0.95'], country)
+            interval_lengths[i][j][0] = pred_lengths67.iloc[-1, 0]
+            interval_lengths[i][j][1] = pred_lengths90.iloc[-1, 0]
+            cov[i][j][0] = probability_coverage(y, preds[f'{country}_q_0.16'], preds[f'{country}_q_0.84'])
+            cov[i][j][1] = probability_coverage(y, preds[f'{country}_q_0.05'], preds[f'{country}_q_0.95'])
+    
+    cov_ratios = cov[:,:,0]/interval_lengths[:,:,0]+cov[:,:,1]/interval_lengths[:,:,1]
     means = np.mean(cov_ratios, axis=1)
     ind = np.argmax(means)
     config = configurations[ind]

Utils/VisualUtils.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 
 import networkx as nx
 
+from SCC_Replication.Variables.State import H
+
 def show_multiple_countries(
         codes: list[str],
         names: list[str],
@@ -300,19 +302,20 @@ def plot_arima_orders(arima_orders: np.ndarray):
     plt.ylabel("Frequency")
     plt.title("Frequency of Values")
     plt.legend()
-    plt.show()    
+    plt.show()
 
 def plot_forecast_intervals(
         data_train: pd.Series,
         data_test: pd.Series,
         country: str,
-        test_predictions: pd.DataFrame,
-        oos_predictions: pd.DataFrame,
-        alpha:float,
-        baseline: dict = None,
+        in_sample: pd.DataFrame = None,
+        test_preds: pd.DataFrame = None,
+        horizon_preds: pd.DataFrame = None,
         ax = None,
         title: str = None,
-        show_legend: bool = True
+        show_legend: bool = True,
+        show67: bool = True,
+        show90: bool = True
     ):
     """
     Plots the probabilistic forecast for a given country
@@ -321,26 +324,17 @@ def plot_forecast_intervals(
         data_train (pd.Series): The training part of the GDP time series
         data_test (pd.Series): The testing_part of the GDP time series
         country (str): The name of the country
-        test_predictions (pd.DataFrame): The prediction intervals on the test set
-        oos_predictions (pd.DataFrame): The out-of-sample (horizon) prediction intervals
-        alpha (float): The percentage of the prediction intervals
-        baseline (dict): The baseline values for the end of the horizon
+        in_sample (pd.DataFrame): The in-sample predictions for the time series
+        test_preds (pd.DataFrame): The predictions for the test set
+        horizon_preds (pd.DataFrame): The future predictions
         ax: The object to be used for plotting
         title (str): The title of the plot; if none, the name of the country
-        show_legend (bool): Whether or not to display a legend alongside the plot.
+        show_legend (bool): Whether or not to display a legend alongside the plot. 
+        show67 (bool): Flag indicating whether or not to plot the 67% prediction intervals
+        show90 (bool): Flag indicating whether or not to plot the 90% prediction intervals           
     """
     if ax is None:
         fig, ax = plt.subplots(figsize=(10, 5))
-
-    last_known_ind = data_test.index[-1]
-    last_known_val = data_test.iloc[-1]
-    last_horizon_ind = oos_predictions.index[-1]
-
-    tr_mean = data_train.mean()
-
-    extended_ind = pd.date_range(start = last_known_ind, end = last_horizon_ind+pd.Timedelta(days=365*3), freq='Y')
-    const_series = pd.Series([tr_mean]*len(extended_ind), index=extended_ind)
-    const_series.plot(ax=ax, label='_nonlegend_', color='none')
 
     if title is None:
         ax.set_title(country)
@@ -348,91 +342,134 @@ def plot_forecast_intervals(
         ax.set_title(title)
 
     data_train.plot(ax=ax, label='Train', color='black')
-    data_test.plot(ax=ax, label="Test", color='g')
+    data_test.plot(ax=ax, label="Test", color='green')
 
-    ax.fill_between(
-        test_predictions.index,
-        test_predictions['lower_bound'],
-        test_predictions['upper_bound'],
-        color = 'lightskyblue',
-        alpha = 0.3,
-        label = f'{alpha}% interval (test)'
-    )
+    if in_sample is not None:
+        if show67:
+            ax.fill_between(
+                in_sample.index,
+                in_sample[f'{country}_q_0.16'],
+                in_sample[f'{country}_q_0.84'],
+                color = 'lightskyblue',
+                alpha=0.8,
+                label = f'67% interval (in-sample)'
+            )
+        if show90:
+            ax.fill_between(
+                in_sample.index,
+                in_sample[f'{country}_q_0.05'],
+                in_sample[f'{country}_q_0.95'],
+                color = 'lightskyblue',
+                alpha=0.3,
+                label = f'90% interval (in-sample)'
+            )
 
-    test_predictions['median'].plot(ax=ax, color='r', label='Median')
-    test_predictions['mean'].plot(ax=ax, color='blueviolet', label='Mean')
-    oos_predictions['median'].plot(ax=ax, color='r', label='_nonlegend_')
-    oos_predictions['mean'].plot(ax=ax, color='blueviolet', label='_nonlegend_')
-    
-    ax.fill_between(
-        oos_predictions.index,
-        oos_predictions['lower_bound'],
-        oos_predictions['upper_bound'],
-        color = 'coral',
-        alpha = 0.8,
-        label = f'{alpha}% interval (horizon)'
-    )
-
-    if baseline is not None:
-        x = pd.date_range(start=last_known_ind, end=last_horizon_ind, freq='Y')
-
-        y_median = np.linspace(last_known_val, baseline['median'], num=len(x))
-        y_mean = np.linspace(last_known_val, baseline['median'], num=len(x))
-        
-        y_median = pd.Series(y_median, index=x)
-        y_mean = pd.Series(y_mean, index=x)
-
-        y_median.plot(ax=ax, color='lime', linestyle='--', label = 'Baseline Median')
-        y_mean.plot(ax=ax, color='teal', linestyle='--', label = 'Baseline Mean')
-
+    if test_preds is not None:
+        if show67:
+            ax.fill_between(
+                test_preds.index,
+                test_preds[f'{country}_q_0.16'],
+                test_preds[f'{country}_q_0.84'],
+                color = 'orange',
+                alpha=0.8,
+                label = f'67% interval (test)'
+            )
+        if show90:
+            ax.fill_between(
+                test_preds.index,
+                test_preds[f'{country}_q_0.05'],
+                test_preds[f'{country}_q_0.95'],
+                color = 'orange',
+                alpha=0.3,
+                label = f'90% interval (test)'
+            )
+    if horizon_preds is not None:
+        horizon_preds[f'{country}_q_0.5'].plot(ax=ax, color='red', label = 'Median (horizon)')
+        horizon_preds[f'{country}_mean'].plot(ax=ax, color='blueviolet', label='Mean (horizon)')
+        if show67:
+            ax.fill_between(
+                horizon_preds.index,
+                horizon_preds[f'{country}_q_0.16'],
+                horizon_preds[f'{country}_q_0.84'],
+                color = 'coral',
+                alpha=0.8,
+                label = f'67% interval (horizon)'
+            )
+        if show90:
+            ax.fill_between(
+                horizon_preds.index,
+                horizon_preds[f'{country}_q_0.05'],
+                horizon_preds[f'{country}_q_0.95'],
+                color = 'coral',
+                alpha=0.3,
+                label = f'90% interval (horizon)'
+            )
 
     if show_legend:
-        ax.legend()
+        ax.legend()  
 
 def plot_many_predictions(
         data_train: pd.DataFrame,
         data_test: pd.DataFrame,
         country: str,
-        test_predictions: list[pd.DataFrame],
-        oos_predictions: list[pd.DataFrame],
-        alpha: float,
-        baseline: dict,
         titles: list[str],
         rows: int,
-        columns: int
+        columns: int,
+        in_sample_preds: list[pd.DataFrame] = None,
+        test_preds: list[pd.DataFrame] = None,
+        horizon_preds: list[pd.DataFrame] = None,
+        show67: bool = True,
+        show90: bool = True,
+        title: str = None
     ):
     """
-    Plots multiple predictions for the same country
+    Plot multiple predictions for the same country
 
     Parameters:
         data_train (pd.Series): The training part of the GDP time series
         data_test (pd.Series): The testing_part of the GDP time series
-        country (str): The name of the country
-        test_predictions (list[pd.DataFrame]): The prediction intervals on the test set
-        oos_predictions (list[pd.DataFrame]): The out-of-sample (horizon) prediction intervals
-        alpha (float): The percentage of the prediction intervals
-        baseline (dict): The baseline values for the end of the horizon
+        country (str): The name of the country    
         titles (list[str]): The titles of each of the subplots
         rows (int): The number of rows of plots
-        columns (int): The number of columns of plots    
+        columns (int): The number of columns of plots
+        in_sample (list[pd.DataFrame]): The in-sample predictions for the time series
+        test_preds (list[pd.DataFrame]): The predictions for the test set
+        horizon_preds (list[pd.DataFrame]): The future predictions
+        show67 (bool): Flag indicating whether or not to plot the 67% prediction intervals
+        show90 (bool): Flag indicating whether or not to plot the 90% prediction intervals  
+        title (str): The title of the plot; if none, the name of the country        
     """
     fig, axs = plt.subplots(rows, columns, figsize=(12,8))
 
     ax_list = axs.flatten()
     for i, ax in enumerate(ax_list):
+        if in_sample_preds is not None:
+            in_sample = in_sample_preds[i]
+        else:
+            in_sample = None
+        if test_preds is not None:
+            test = test_preds[i]
+        else:
+            test = None
+        if horizon_preds is not None:
+            horizon = horizon_preds[i]
+        else:
+            horizon = None
+
         plot_forecast_intervals(
-            data_train=data_train,
-            data_test=data_test,
-            country=country,
-            test_predictions=test_predictions[i],
-            oos_predictions=oos_predictions[i],
-            alpha=alpha,
-            baseline=baseline,
+            data_train, 
+            data_test, 
+            country, 
+            in_sample=in_sample, 
+            test_preds=test, 
+            horizon_preds=horizon,
             ax=ax,
             title=titles[i],
-            show_legend=False
+            show_legend=False,
+            show67=show67, 
+            show90=show90
         )
-    
+
     handles, labels = [], []
     ax = ax_list[0]
     for handle, label in zip(*ax.get_legend_handles_labels()):
@@ -440,117 +477,11 @@ def plot_many_predictions(
         labels.append(label)
 
     fig.legend(handles, labels, loc='lower center', ncol=6, fontsize=10)
-
-    fig.suptitle(country, fontsize=16, weight='bold')
-    plt.tight_layout(rect=[0,0.05,1,0.92])
-    plt.show()
-
-def plot_predictions_both_intervals(
-        data_train: pd.Series,
-        data_test: pd.Series,
-        country: str,
-        oos_predictions67: pd.DataFrame,
-        oos_predictions90: pd.DataFrame,
-        ax = None,
-        title: str = None,
-        show_legend: bool = True
-    ):
-    """
-    Plots the probabilistic forecast for a given country
-
-    Parameters:
-        data_train (pd.Series): The training part of the GDP time series
-        data_test (pd.Series): The testing_part of the GDP time series
-        country (str): The name of the country
-        oos_predictions67 (pd.DataFrame): The out-of-sample (horizon)  67% prediction intervals
-        oos_predictions67 (pd.DataFrame): The out-of-sample (horizon)  90% prediction intervals
-        ax: The object to be used for plotting
-        title (str): The title of the plot; if none, the name of the country
-        show_legend (bool): Whether or not to display a legend alongside the plot.
-    """
-    if ax is None:
-        fig, ax = plt.subplots(figsize=(10, 5))
 
     if title is None:
-        ax.set_title(country)
+        fig.suptitle(country, fontsize=16)
     else:
-        ax.set_title(title)
-
-    data_train.plot(ax=ax, label='Train', color='black')
-    data_test.plot(ax=ax, label="Test", color='g')
-
-    oos_predictions67['median'].plot(ax=ax, color='r', label='_nonlegend_')
-    oos_predictions67['mean'].plot(ax=ax, color='blueviolet', label='_nonlegend_')
-    
-    ax.fill_between(
-        oos_predictions67.index,
-        oos_predictions67['lower_bound'],
-        oos_predictions67['upper_bound'],
-        color = 'coral',
-        alpha = 0.8,
-        label = f'67% interval (horizon)'
-    )
-
-    ax.fill_between(
-        oos_predictions90.index,
-        oos_predictions90['lower_bound'],
-        oos_predictions90['upper_bound'],
-        color = 'coral',
-        alpha = 0.3,
-        label = f'90% interval (horizon)'
-    )
-
-
-    if show_legend:
-        ax.legend()
-
-def plot_many_predictions_both_intervals(
-        data_train: pd.DataFrame,
-        data_test: pd.DataFrame,
-        country: str,
-        oos_predictions67: list[pd.DataFrame],
-        oos_predictions90: list[pd.DataFrame],
-        titles: list[str],
-        rows: int,
-        columns: int
-    ):
-    """
-    Plots multiple predictions for the same country
-
-    Parameters:
-        data_train (pd.Series): The training part of the GDP time series
-        data_test (pd.Series): The testing_part of the GDP time series
-        country (str): The name of the country
-        oos_predictions67 (list[pd.DataFrame]): The out-of-sample (horizon) 67% prediction intervals
-        oos_predictions90 (list[pd.DataFrame]): The out-of-sample (horizon) 90% prediction intervals
-        titles (list[str]): The titles of each of the subplots
-        rows (int): The number of rows of plots
-        columns (int): The number of columns of plots    
-    """
-    fig, axs = plt.subplots(rows, columns, figsize=(12,8))
-
-    ax_list = axs.flatten()
-    for i, ax in enumerate(ax_list):
-        plot_predictions_both_intervals(
-            data_train=data_train,
-            data_test=data_test,
-            country=country,
-            oos_predictions67=oos_predictions67[i],
-            oos_predictions90=oos_predictions90[i],
-            ax=ax,
-            title = titles[i],
-            show_legend=False
-        )
-    
-    handles, labels = [], []
-    ax = ax_list[0]
-    for handle, label in zip(*ax.get_legend_handles_labels()):
-        handles.append(handle)
-        labels.append(label)
-
-    fig.legend(handles, labels, loc='lower center', ncol=6, fontsize=10)
-
-    fig.suptitle(country, fontsize=16, weight='bold')
+        fig.suptitle(title, fontsize=16)
     plt.tight_layout(rect=[0,0.05,1,0.92])
     plt.show()
 
@@ -562,12 +493,12 @@ def plot_prediction_and_baseline(
         T: int,
         gdp: np.ndarray,
         low_freq: np.ndarray,
-        pred67: pd.DataFrame,
-        pred90: pd.DataFrame,
+        horizon_preds: pd.DataFrame,
         q05: np.ndarray,
         q16: np.ndarray,
         q84: np.ndarray,
         q95: np.ndarray,
+        title: str = None
     ):
     """
     Plot the prediction intervals of a specific model and the baseline on the same plot
@@ -580,12 +511,12 @@ def plot_prediction_and_baseline(
         T (int): The duration of the observed data
         gdp (np.ndarray): The observed GDP data for all countries
         low_freq (np.ndarray): The low-frequency trend of the observed data
-        pred67 (pd.DataFrame): The 67% prediction intervals the model has produces for the desired country
-        pred90 (pd.DataFrame): The 90% prediction intervals the model has produces for the desired country
+        horizon_preds (pd.Dataframe): The future predictions
         q05 (np.ndarray): The 5th baseline quantile
         q16 (np.ndarray): The 16th baseline quantile
         q84 (np.ndarray): The 84th baseline quantile
         q95 (np.ndarray): The 95th baseline quantile
+        title (str): The title of the plot; if none, the name of the country  
     """
     T_horizon = len(q05[:,0])
     in_sample_time = np.arange(start_year, start_year+T)
@@ -601,7 +532,7 @@ def plot_prediction_and_baseline(
         horizon_time, q16[:, ind], q84[:, ind], color='lightskyblue', alpha=0.5, label='Baseline Prediction Interval'
     )
     ax.fill_between(
-        horizon_time, pred67['lower_bound'], pred67['upper_bound'], color='coral', alpha=0.5, label=f'{model_type} Prediction Interval'
+        horizon_time, horizon_preds[f'{country}_q_0.16'], horizon_preds[f'{country}_q_0.84'], color='coral', alpha=0.5, label=f'{model_type} Prediction Interval'
     )
     ax.set_title(f'67% Prediction Intervals')
 
@@ -612,7 +543,7 @@ def plot_prediction_and_baseline(
         horizon_time, q05[:, ind], q95[:, ind], color='lightskyblue', alpha=0.5, label='Baseline Prediction Interval'
     )
     ax.fill_between(
-        horizon_time, pred90['lower_bound'], pred90['upper_bound'], color='coral', alpha=0.5, label=f'{model_type} Prediction Interval'
+        horizon_time,  horizon_preds[f'{country}_q_0.05'], horizon_preds[f'{country}_q_0.95'], color='coral', alpha=0.5, label=f'{model_type} Prediction Interval'
     )
     ax.set_title(f'90% Prediction Intervals')
 
@@ -622,20 +553,9 @@ def plot_prediction_and_baseline(
         labels.append(label)
     
     fig.legend(handles, labels, loc='lower center', ncol=6, fontsize=10)
-    fig.suptitle(country, fontsize=16, weight='bold')
+    if title is None:
+        fig.suptitle(country, fontsize=16)
+    else:
+        fig.suptitle(title, fontsize=16)
     plt.tight_layout(rect=[0,0.05,1,0.92])
     plt.show()
-
-# def plot_predictions_all_samples(
-#         country: str,
-#         univariate: bool,
-#         start_year: int,
-#         T: int,
-#         T_train: int,
-#         gdp: np.ndarray,
-#         in_sample_preds: pd.DataFrame,
-#         test_preds67: pd.DataFrame,    
-#         horizon_preds67: pd.DataFrame,
-#         test_preds90: pd.DataFrame,
-#         horizon_preds90: pd.DataFrame
-#     ):